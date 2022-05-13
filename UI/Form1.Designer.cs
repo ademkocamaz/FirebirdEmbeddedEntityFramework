e439edd022fef715154c4d8a4adb37ff62a545b1
Adem KOCAMAZ <kocamazadem@gmail.com>
2022-05-13 12:53:26 +0300

    Test Context

--- UI/Form1.Designer.cs
+++ UI/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
@@ -42,11 +43,11 @@
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(9, 10);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(582, 454);
+            this.dataGridView1.Size = new System.Drawing.Size(566, 408);
             this.dataGridView1.TabIndex = 0;
             // 
             // dataGridView2
@@ -54,19 +55,30 @@
             this.dataGridView2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(9, 469);
+            this.dataGridView2.Location = new System.Drawing.Point(9, 423);
             this.dataGridView2.Name = "dataGridView2";
-            this.dataGridView2.Size = new System.Drawing.Size(582, 177);
+            this.dataGridView2.Size = new System.Drawing.Size(566, 177);
             this.dataGridView2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(497, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 658);
+            this.ClientSize = new System.Drawing.Size(584, 612);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.dataGridView1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
@@ -81,6 +93,7 @@
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- UI/Form1.cs
+++ UI/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using Business.Abstract;
 using Business.DependencyResolvers.Ninject;
+using DataAccess.Concrete.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -28,6 +29,18 @@ namespace UI
         {
             dataGridView1.DataSource = noteService.GetList();
             dataGridView2.DataSource = categoryService.GetList();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //var entity = noteService.Get(x => x.Id.Equals(4));
+            //MessageBox.Show(entity.Category.Name);
+
+            using (var context = new NotesContext())
+            {
+                var note = context.Notes.Where(x => x.Id == 4).FirstOrDefault();
+                MessageBox.Show(context.Categories.Where(x=>x.Id==note.CategoryId).FirstOrDefault().Name);
+            }
         }
     }
 }
